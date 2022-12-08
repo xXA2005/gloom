5a70f80880da5f4b90e3360c6e09858cfc754859
xXA2005 <A2005BALILA@gmail.com>
2022-12-09 00:18:31 +0300

    Updates

---
+++ precache-manifest.ed7f6b5946de07b1ca354dccaf0e0699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d031a248bc8856986e8",
+    "url": "/gloom/static/css/main.77360fc5.chunk.css"
+  },
+  {
+    "revision": "4d031a248bc8856986e8",
+    "url": "/gloom/static/js/main.4d031a24.chunk.js"
+  },
+  {
+    "revision": "31ed03dcec6d39ac7add",
+    "url": "/gloom/static/js/1.31ed03dc.chunk.js"
+  },
+  {
+    "revision": "137b6bcc6d3bbb334eaa",
+    "url": "/gloom/static/js/runtime~main.137b6bcc.js"
+  },
+  {
+    "revision": "6da48ddd8588018bcbed6f65adb68f14",
+    "url": "/gloom/static/media/image.6da48ddd.png"
+  },
+  {
+    "revision": "f134cb60609bae963f7078d9657b513e",
+    "url": "/gloom/index.html"
+  }
+];
