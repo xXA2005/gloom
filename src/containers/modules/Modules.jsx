3d7a245acc35c2d37d2c50e1540e00afe735b3fd
xXA2005 <A2005BALILA@gmail.com>
2022-12-10 22:09:20 +0300

    a

--- src/containers/modules/Modules.jsx
+++ src/containers/modules/Modules.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './modules.css'
 
-const card = (m) => {
-  return (
-    <div class="card">
-      <h3>{m[0]}</h3>
-      <p>{m[1]}</p>
-    </div>
+const renderCards = (ms) => {
+  return ms.map(m => 
+  <div class="card">
+    <h3>{m[0]}</h3>
+    <p>{m[1]}</p>
+  </div>
   )
 }
 
@@ -40,27 +40,7 @@ const Projects = () => {
             <div className='item'>
                     <a>Features</a>
                   <div className='item-about'>
-                  {card(mods[0])}
-                  {card(mods[1])}
-                  {card(mods[2])}
-                  {card(mods[3])}
-                  {card(mods[4])}
-                  {card(mods[5])}
-                  {card(mods[6])}
-                  {card(mods[7])}
-                  {card(mods[8])}
-                  {card(mods[9])}
-                  {card(mods[10])}
-                  {card(mods[11])}
-                  {card(mods[12])}
-                  {card(mods[13])}
-                  {card(mods[14])}
-                  {card(mods[15])}
-                  {card(mods[16])}
-                  {card(mods[17])}
-                  {card(mods[18])}
-                  {card(mods[19])}
-
+                  {renderCards(mods)}
                   </div>
             </div>
 
